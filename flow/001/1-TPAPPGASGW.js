5ec2281665ba619a5c38efffb561e137781adb0f
Thai Parkerizing <administrator@Thais-MacBook-Pro.local>
2025-03-31 19:46:05 +0700

    .

--- flow/001/1-TPAPPGASGW.js
+++ flow/001/1-TPAPPGASGW.js
@@ -187,7 +187,7 @@ router.post('/GETINtoAPPGASGW', async (req, res) => {
           "PARTNAME_PO": dbsap['PARTNAME_PO'] || '',
           "PART_PO": dbsap['PART_PO'] || '',
           "CUSTNAME_s": dbsap['CUSTNAME'] || '',
-          "CUSTNAME": findcp[0]['CUST_FULLNM'] || '',
+          "CUSTNAME": dbsap['CUST_FULLNM']|| '',
           "UNITSAP": dbsap['UNIT'] || '',
           //----------------------
           "ItemPick": ItemPickoutP2, //---->
