dd9aa350410564096bde2586322f31d2945f1a9f
Thai Parkerizing <administrator@Thais-MacBook-Pro.local>
2026-03-15 14:42:59 +0700

    .

--- function/axios.js
+++ function/axios.js
@@ -1,34 +1,26 @@
 const axios = require('axios')
 
-exports.post =  async (url,body) => {
-    await axios.post( url, body).then(async res => {
-    // console.log(`statusCode: ${res.status}`)
-    // console.log(res)
-    // console.log(res.data);
-    output =  res.data  
-  })
-  .catch(async error => {
-    console.error(error.response.status)
-    output = await error.response.status
-    // outputr = `err`
-  })
-  return output;
-};
+const instance = axios.create({
+  timeout: 30000,
+  headers: { 'Connection': 'keep-alive' }
+})
 
-exports.get =  async (url) => {
-    
-    await axios.get(url).then(async res => {
-    // console.log(`statusCode: ${res.status}`)
-    // console.log(res)
-    // console.log(res.data);
-    output =  res.data  
-  })
-  .catch(async error => {
-    console.error(error.response.status)
-    output = await error.response.status
-    // outputr = `err`
-  })
+exports.post = async (url, body) => {
+  try {
+    const res = await instance.post(url, body)
+    return res.data
+  } catch (error) {
+    console.error(error?.response?.status ?? error.message)
+    return error?.response?.status ?? null
+  }
+}
 
-  return output;
-     
-};
+exports.get = async (url) => {
+  try {
+    const res = await instance.get(url)
+    return res.data
+  } catch (error) {
+    console.error(error?.response?.status ?? error.message)
+    return error?.response?.status ?? null
+  }
+}
